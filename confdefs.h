a151fde8afebdb916707a480f1ca96365706bcee
mdenwood <md@sund.ku.dk>
2022-10-20 12:27:38 +0200

    Update docs

---
+++ confdefs.h
@@ -0,0 +1,7 @@
+/* confdefs.h */
+#define PACKAGE_NAME "runjags"
+#define PACKAGE_TARNAME "runjags"
+#define PACKAGE_VERSION "2"
+#define PACKAGE_STRING "runjags 2"
+#define PACKAGE_BUGREPORT ""
+#define PACKAGE_URL ""
